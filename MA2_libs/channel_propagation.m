4d69884a260159deacdb20c3ec3fd676e7d496ac
cyprienmaes <cyprienmaes@gmail.com>
2020-02-28 11:39:37 +0100

    Rayleigh channel 3

--- MA2_libs/channel_propagation.m
+++ MA2_libs/channel_propagation.m
@@ -22,14 +22,18 @@ function signal_rx = channel_propagation(params,signal_tx,SNR)
     signal_tx_col = reshape(signal_tx,Lcp+Q,[]);
     impulse_response = zeros(Lcp+Q,1);
     impulse_response(1) = 1;
-    a = (rand(1,Lcp+Q-1)).';
-    phi = 2*pi*(randn(1,Lcp+Q-1)).';
+    %a = (rand(1,Lcp+Q-1)).';
+    a = rand(1,1);
+    %phi = 2*pi*(randn(1,Lcp+Q-1)).';
+    phi = 2*pi*randn(1,1);
+    % phi = 0;
     % map it to the range [0,2*pi]
-    phi = 0;%mod(phi,2*pi);
-    impulse_response(2:end) = a.*exp(1i*phi);
+    phi = mod(phi,2*pi);
+    impulse_response(randi([1,Lcp+Q])) = a*exp(1i*phi);
+    %impulse_response(2) = 0.2;
     impulse_matrix = convolutionMatrix(impulse_response);
     signal_rx = impulse_matrix*signal_tx_col;
-    signal_rx = reshape(signal_rx.',[],size(signal_rx,1)*size(signal_rx,2));
+    signal_rx = reshape(signal_rx,size(signal_rx,1)*size(signal_rx,2),1).';
     
 
     transmitted_energy = norm(signal_tx(:))^2;           % energy of the signal
@@ -37,11 +41,14 @@ function signal_rx = channel_propagation(params,signal_tx,SNR)
     noise_var = noise_energy/(length(signal_tx(:))-1);   % variance of noise to be added
     noise_std = sqrt(noise_var/2);                       % std. deviation of noise to be added
     noise = noise_std*(randn(length(signal_tx),1)+1i*randn(length(signal_tx),1));      % noise
-    signal_rx = (signal_rx).'+noise; 
-%     % Matched filter + MMSE equalizer
+    signal_rx = signal_rx+noise.'; 
+    % Matched filter + MMSE equalizer
 %     Nb = 2 * params.ofdm.N_subcrr * params.modulation.Nbps;
 %     No = noise_energy/(Nb*2);
-%     signal_rx = ((2*No/var(signal_tx)+impulse_matrix'*impulse_matrix)\impulse_matrix'*signal_rx).';
+%     signal_rx_col = reshape(signal_rx,Lcp+Q,[]);
+%     var1 = var(signal_rx);
+%     signal_rx_col = (2*No/var1+impulse_matrix'*impulse_matrix)\impulse_matrix'*signal_rx_col;
+%     signal_rx = reshape(signal_rx_col,size(signal_rx,1)*size(signal_rx,2),1).';
    
     
     % ---------------------------------------------------------------------
